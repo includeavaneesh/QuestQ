dc1dd8415d629fcf10e8b590ba08d9e042372aa5
includeavaneesh <avaneeshkanshi5@gmail.com>
2024-06-13 10:18:12 +0530

    Something

--- src/main/java/com/questq/main/controller/QHomeController.java
+++ src/main/java/com/questq/main/controller/QHomeController.java
@@ -1,13 +1,23 @@
 package com.questq.main.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.questq.main.Author;
+import com.questq.main.Book;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
 
-@RestController
+
+@Controller
 public class QHomeController {
 
-    @GetMapping("/")
-    public String getHome() {
-        return "Hello";
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.authorId());
     }
 }
